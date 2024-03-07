ab2c4c043e90068414da26830db9eea85b71c8ce
cat24a <artur.2010@icloud.com>
2024-03-07 18:56:43 +0100

    add password change functionality to api

--- public/api.php
+++ public/api.php
@@ -12,6 +12,7 @@ if(isset($request->user)) $request->user = base64_decode($request->user);
 if(isset($request->pass)) $request->pass = base64_decode($request->pass);
 if(isset($request->session)) $request->session = base64_decode($request->session);
 if(isset($request->data)) $request->data = base64_decode($request->data);
+if(isset($request->newpass)) $request->newpass = base64_decode($request->newpass);
 
 switch ($request->action) {
     case 'new_session':
@@ -41,6 +42,21 @@ switch ($request->action) {
     case "add_user":
         $query = $db->prepare("INSERT INTO `userdata`(`name`, `pass`, `data`) VALUES (:user, :pass, :data)");
         $query->execute(["user" => $request->user, "pass" => $request->pass, "data" => $request->data]);
+        echo "{}";
+        break;
+
+    case "change_pass":
+        if(isset($request->data)) {
+            $query = $db->prepare("UPDATE `userdata` SET `data`=:data `pass`=:newpass WHERE `name`=:user AND `pass`=:pass AND `sess`=:session");
+            $query->execute(["user" => $request->user, "pass" => $request->pass, "session" => $request->session, "data" => $request->data, "newpass" => $request->newpass]);
+        } else {
+            $query = $db->prepare("UPDATE `userdata` SET `pass`=:newpass WHERE `name`=:user AND `pass`=:pass AND `sess`=:session");
+            $query->execute(["user" => $request->user, "pass" => $request->pass, "session" => $request->session, "newpass" => $request->newpass]);
+        }
+        if(!$query->rowCount()) {
+            echo json_encode(["error" => "wrong_data"]);
+            exit();
+        }
         echo "{}";
         break;
 
